6b6020081db36b96f565a8107ef69096a77b746c
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-03-28 10:22:59 -0500

    Se agrego un modo oscuro en la aplicación

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Flex, Grid, Image, Text, Input, Icon, Button } from '@chakra-ui/react'
+import { useColorMode, Flex, Grid, Image, Text, Input, Icon, Button, InputLeftElement, InputGroup, Stack } from '@chakra-ui/react'
 import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa6";
+import { FaSun } from "react-icons/fa";
+import { IoSearchSharp, IoMoon } from "react-icons/io5";
 import Logo from "../images/logo2.png"
 
 const NavBar2 = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
     const buttonStyle = {
         borderRadius: '0.5em',
         marginLeft: 'auto',
@@ -28,21 +31,29 @@ const NavBar2 = () => {
                 zIndex="99999"
             >
                 <Grid templateColumns="repeat(4, 1fr)" gap={6} p={4} w="100%">
-                    <Flex cursor="pointer">
+                    <Flex cursor="pointer" pt="0.2em" w="20%">
                         <Image src={Logo} alt="logo" w="30px" h="30px" />
                         <Text fontSize="xl" fontWeight="bold" ml="0.2em">Puertas</Text>
                     </Flex>
-                    <Input placeholder="Buscar" size="md" />
-                    <Flex justifyContent="flex-end">
+                    <Stack spacing={4}>
+                        <InputGroup>
+                            <InputLeftElement pointerEvents='none'>
+                               <IoSearchSharp />
+                            </InputLeftElement>
+                            <Input placeholder="Buscar" size="md" />
+                        </InputGroup>
+                    </Stack>
+                    <Flex justifyContent="flex-end" pt="0.5em">
                         <Icon as={FaFacebook} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.facebook.com')} />
                         <Icon as={FaInstagram} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.instagram.com')} />
-                        <Icon as={FaWhatsapp} boxSize="1.5em" sx={socialMediaStyle} onClick={() => window.open('https://www.whatsapp.com')} />
+                        <Icon as={FaWhatsapp} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.whatsapp.com')} />
+                        <Icon as={colorMode === 'light' ? IoMoon : FaSun} boxSize="1.5em" sx={socialMediaStyle} onClick={toggleColorMode} title={colorMode === 'light' ? 'Dark' : 'Light'}/>
                     </Flex>
                     <Button variant="custom" sx={buttonStyle} leftIcon={<FaWhatsapp />} onClick={handleClickWhatsAppButton}>
                         WhatsApp
                     </Button>
                 </Grid>
-            </Flex>
+            </Flex >
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from './components/ThemeChange';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
