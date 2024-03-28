be9657ac90d4c93bc1ef31d75f6e88b79c92fb76
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-03-28 17:09:59 -0500

    Se elimino el primer navbar que habia sido programada por temas de estetica, el navbar actual es mucho mejor. Se agrega la carpeta de images con algunos recursos en su interior

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,68 +1,86 @@
-import { Flex, Text, Button, useMediaQuery, Collapse, Icon, Image } from '@chakra-ui/react'
-import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa6";
-import { RiMenu3Fill, RiCloseLine } from "react-icons/ri";
 import React, { useState } from 'react'
-import Logo from '../images/logo.png'
+import {
+    useMediaQuery,
+    useColorMode,
+    Flex,
+    Grid,
+    Image,
+    Input,
+    Icon,
+    Button,
+    InputLeftElement,
+    InputGroup,
+    Stack
+} from '@chakra-ui/react'
+import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa6";
+import { FaSun } from "react-icons/fa";
+import { IoSearchSharp, IoMoon, IoClose, IoMenu } from "react-icons/io5";
+import Logo from "../images/logo.png"
+import NombreNegocioDark from "../images/nombre-negocio-dark.png"
+import NombreNegocioLight from "../images/nombre-negocio-light.png"
 
 const NavBar = () => {
-
+    const [isLargerThan1256] = useMediaQuery("(min-width: 1256px)")
+    const [isLargerThan860] = useMediaQuery("(min-width: 860px)")
+    const [isLargerThan640] = useMediaQuery("(min-width: 640px)")
+    const { colorMode, toggleColorMode } = useColorMode()
     const buttonStyle = {
         borderRadius: '0.5em',
         marginLeft: 'auto',
         bg: 'green.500',
         color: 'white',
-        _hover: { bg: 'green.600' },
+        _hover: {
+            transform: "scale(1.05, 1.05)",
+            bg: 'green.600'
+        },
         _active: { bg: 'green.700' },
     }
-    const textStyle = {
-        color: 'orange.400',
-        fontSize: 'md',
-        fontWeight: 'bold',
-        _hover: { color: 'orange.500' },
-        _active: { color: 'orange.600' },
+    const handleClickWhatsAppButton = () => {
+        window.open('https://www.whatsapp.com')
     }
     const socialMediaStyle = {
         cursor: 'pointer',
         _hover: { color: 'orange.500' },
         _active: { color: 'orange.600' },
     }
-    const handleClickWhatsAppButton = () => {
-        window.open('https://www.whatsapp.com')
-    }
-    const [isLargerThan768] = useMediaQuery("(min-width: 628px)")
     const [show, setShow] = useState(false)
     const handleToggleShowCollapse = () => setShow(!show);
-
     return (
         <>
-            <Flex display={isLargerThan768 ? 'flex' : 'none'} zIndex="99999" alignItems="center" justifyContent="flex-start" p="1em" bg="black">
-                <Text sx={textStyle} marginRight="2rem"><a href='/'>Inicio</a></Text>
-                <Text sx={textStyle} marginRight="2rem"><a href='/catalogo'>Catálogo</a></Text>
-                <Text sx={textStyle} marginRight="2rem"><a href='/contacto'>Contacto</a></Text>
-                <Text sx={textStyle}><a href='/sobre-nosotros'>Sobre Nosotros</a></Text>
-                <Button variant="custom" sx={buttonStyle} leftIcon={<FaWhatsapp />} onClick={handleClickWhatsAppButton}>
-                    WhatsApp
-                </Button>
-            </Flex>
-            <Flex display={isLargerThan768 ? 'flex' : 'none'} alignItems="center" justifyContent="flex-start" p="1em" pl="2em">
-                <Icon as={FaFacebook} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.facebook.com')} />
-                <Icon as={FaInstagram} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.instagram.com')} />
-                <Icon as={FaWhatsapp} boxSize="1.5em" sx={socialMediaStyle} onClick={() => window.open('https://www.whatsapp.com')} />
-                <Image src={Logo} alt='Logo' margin="auto" />
-            </Flex>
-            <Flex direction="column" display={isLargerThan768 ? 'none' : 'flex'} zIndex="99999" alignItems="left" p="1em" bg="black" textAlign="left">
-
-                <Flex direction="row" justifyContent="space-between" >
-                    <Image src={Logo} alt='Logo' />
-                    <Button leftIcon={show ? <RiCloseLine /> : <RiMenu3Fill />} onClick={handleToggleShowCollapse} mt="0.3em" pr="0.5em"/>
-                </Flex>
-                <Collapse in={show} animateOpacity>
-                    <Text sx={textStyle} mt="2em" ><a href='/'>Inicio</a></Text>
-                    <Text sx={textStyle} mt="2em"><a href='/catalogo'>Catálogo</a></Text>
-                    <Text sx={textStyle} mt="2em"><a href='/contacto'>Contacto</a></Text>
-                    <Text sx={textStyle} mt="2em" mb="1em"><a href='/sobre-nosotros'>Sobre Nosotros</a></Text>
-                </Collapse>
-            </Flex>
+            <Flex
+                borderBottom="1px"
+                borderColor="gray.200"
+                zIndex="99999"
+            >
+                <Grid templateColumns={isLargerThan1256 ? "0.3fr 2fr 0.4fr 0.3fr" : isLargerThan860 ? "0.5fr 2.3fr 0.5fr" : isLargerThan640 ? "0.3fr 2.5fr 0.3fr" : "0.3fr 2.6fr 0.1fr 0.1fr"} gap={6} p={4} w="100%">
+                    <Flex pt="0.2em" justifyContent="center" >
+                        <a href="/"><Image src={Logo} alt="logo" w="30px" h="30px" cursor="pointer" /></a>
+                        {isLargerThan860 && <a href="/"><Image src={colorMode === 'light' ? NombreNegocioLight : NombreNegocioDark} alt="logo" cursor="pointer" /></a>}
+                    </Flex>
+                    <Stack spacing={4}>
+                        <InputGroup>
+                            <InputLeftElement pointerEvents='none'>
+                                <IoSearchSharp />
+                            </InputLeftElement>
+                            <Input placeholder="Buscar puertas..." size="md" />
+                        </InputGroup>
+                    </Stack>
+                    <Flex justifyContent="center" pt="0.5em">
+                        {isLargerThan860 && <Icon as={FaFacebook} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.facebook.com')} />}
+                        {isLargerThan860 && <Icon as={FaInstagram} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.instagram.com')} />}
+                        {isLargerThan860 && <Icon as={FaWhatsapp} boxSize="1.5em" mr="1em" sx={socialMediaStyle} onClick={() => window.open('https://www.whatsapp.com')} />}
+                        <Icon as={colorMode === 'light' ? IoMoon : FaSun} boxSize="1.5em" sx={socialMediaStyle} onClick={toggleColorMode} />
+                    </Flex>
+                    {!isLargerThan640 && <Flex justifyContent="center" pt="0.5em">
+                        <Icon as={show ? IoClose : IoMenu} boxSize="1.5em" onClick={handleToggleShowCollapse} />
+                    </Flex>}
+                    {isLargerThan1256 && <Flex ml="none">
+                        <Button variant="custom" sx={buttonStyle} leftIcon={<FaWhatsapp />} onClick={handleClickWhatsAppButton} mr="0.4em">
+                            WhatsApp
+                        </Button>
+                    </Flex>}
+                </Grid>
+            </Flex >
         </>
     )
 }
