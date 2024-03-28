5e5cf3254beba902bbf60a100f74b215a25f6920
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-03-27 22:56:27 -0500

    Se agrego el navbar al RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import NavBar from "../components/NavBar"
 
 const RootLayout = () => {
   return (
     <div>
+      <NavBar />
       <Outlet />
     </div>
   )
