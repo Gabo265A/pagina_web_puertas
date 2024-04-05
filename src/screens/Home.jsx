6c4b77e017d2f4fc9f6d490c98a805872c5e691b
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-04-05 15:26:28 -0500

    Se empieza a desarrollar el Home, todo actualmente es un borrador

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Product from '../objects/Product'
 
 const Home = () => {
   return (
     <div>
-      Inicio
-    </div>
+      <section>
+        <h1>Puertas y Pintuas AA</h1>
+        <p>Descubre la belleza natural de nuestros productos en madera en Puertas y Pinturas AA. Creamos piezas únicas y funcionales que transforman espacios en lugares acogedores y llenos de estilo. Desde elegantes puertas y ventanas hasta muebles a medida, cada artículo resalta la calidez y elegancia de la madera.</p>
+      </section>
+      <section>
+        <h2>Productos destacados</h2>
+        <ul>
+          <Product />
+        </ul>
+      </section>
+    </div >
   )
 }
 
